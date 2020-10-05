f33e5fe65c38540d45c6029b409d18c14d521b31
hganaqui <hganaqui@gmail.com>
2020-10-04 23:38:32 -0300

    TDD - RED -Escrever casos de teste

---
+++ TDDExample/TestMSTest/EntityTest/PersonTest.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestMSTest.EntityTest
+{
+    [TestClass]
+    public class PersonTest
+    {
+        [TestMethod]
+        public void PeopleIsValid()
+        {
+            string cpf = "12345678901";
+            string name = "Teste Henrique";
+
+            Person person = new Person(cpf, name);
+
+            Assert.IsNotNull(person);
+            Assert.IsTrue(person.Cpf == cpf);
+            Assert.IsTrue(person.Name == name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void PeopleNotCreateCPF()
+        {
+            string cpf = null;
+            string name = "Teste Henrique";
+
+            Person person = new Person(cpf, name);
+
+        }
+    }
+}
